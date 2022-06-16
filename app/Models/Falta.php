2532aa1fa674a882c70a0a273bc3e2991c7f83d2
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-06-15 20:13:12 -0500

    Migracion y Modelo: Falta

---
+++ app/Models/Falta.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Falta extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'fecha',
+        'empleado_id'
+    ];
+}
