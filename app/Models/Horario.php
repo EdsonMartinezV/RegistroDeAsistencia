5ffd304127a27d1dd666f08c2741f11d47cbbfb2
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-06-16 21:57:51 -0500

    Migracion y modelo: Catalogo de horarios

---
+++ app/Models/Horario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Horario extends Model
+{
+    use HasFactory;
+    protected $table = 'catalogo_de_horarios';
+    protected $fillable = [
+        'hora_entrada',
+        'hora_salida'
+    ];
+}
