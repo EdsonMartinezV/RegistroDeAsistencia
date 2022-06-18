564666c00e2717afeee0f784e9bf73360052ffa8
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-06-17 20:55:54 -0500

    Migracion y Modelo: Periodo

---
+++ database/migrations/2022_06_18_015345_create_periodos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periodos', function (Blueprint $table) {
+            $table->id();
+            $table->date('inicio_periodo_laboral');
+            $table->date('fin_periodo_laboral');
+            $table->foreignId('empleado_id')
+                ->constrained('empleados');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periodos');
+    }
+};
