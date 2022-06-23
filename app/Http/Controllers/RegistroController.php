f35f027b68987240c767034e0b3105777997fa8c
KevinEscobarDeveloper <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2022-06-22 20:48:22 -0500

    avance factories

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class RegistroController extends Controller
 {
-    //
+    public function registrarAsistencia(Request $request){
+        
+        Event::where('id',$id)->update(['event_date'=>$request->date,'price'=>$request->price]);
+        $bookings = Event::where('id',$id)->get();
+       
+        return redirect()->route('client.add.image',$id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('asistencia');
 });
+
+//Registrar
+Route::post('/register-user',[RegistroController::class, 'registrarAsistencia'])->name('usuario.asistencia');
