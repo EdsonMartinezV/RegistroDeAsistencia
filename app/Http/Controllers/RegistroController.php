e82fc202a7f1263d95bf728c4e5979d151c16a83
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-06-22 20:52:49 -0500

    Ruta formulario

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class RegistroController extends Controller
 {
-    //
+    public function validarAsistencia(Request $request){
+        $request->validate([
+            'empleado_id' => 'required',
+            'hora' => 'required',
+        ]);
+
+        
+    }
 }
