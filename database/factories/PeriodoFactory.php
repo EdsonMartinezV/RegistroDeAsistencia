91a1a94a93ab5b830ab04c43e09567e0573241ce
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-06-22 22:05:59 -0500

    Periodo Factory

--- database/factories/PeriodoFactory.php
+++ database/factories/PeriodoFactory.php
@@ -17,7 +17,9 @@ class PeriodoFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'inicio_periodo_laboral' => $this->faker->dateTimeBetween('-1 year', '+1 year'),
+            'fin_periodo_laboral' => $this->faker->dateTimeBetween('-1 year', '+1 year'),
+            'empleado_id' => $this->faker->numberBetween(1, 10)
         ];
     }
 }
