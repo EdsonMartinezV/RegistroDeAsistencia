1ede872a2ddf70543e71a8e4810755328e7c927e
KevinEscobarDeveloper <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2022-06-29 23:09:52 -0500

    seeder dias laborales

--- database/seeders/DiaSeeder.php
+++ database/seeders/DiaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Diax;
 
 class DiaSeeder extends Seeder
 {
@@ -14,6 +15,80 @@ class DiaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 1,
+            'dia_salida' => 1,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 2,
+            'dia_salida' => 2,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 3,
+            'dia_salida' => 3,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 4,
+            'dia_salida' => 4,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 5,
+            'dia_salida' => 5,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 6,
+            'dia_salida' => 6,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 7,
+            'dia_salida' => 7,
+        ]);
+
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 1,
+            'dia_salida' => 2,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 3,
+            'dia_salida' => 4,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 5,
+            'dia_salida' => 6,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 1,
+            'dia_salida' => 3,
+        ]);
+        Dia::create([
+            'periodo_id' => 1,
+            'catalogo_de_horarios_id' => '15:30',
+            'dia_entrada' => 5,
+            'dia_salida' => 7,
+        ]);
+        
+        
     }
 }
