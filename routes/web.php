4abc61f8604fab42746803f320859e1e0eb7bede
Missell <98782504+MissellCrz@users.noreply.github.com>
2022-07-02 20:22:30 -0500

    ruta

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('empleado');
+    return view('justificante');
 })->name('formularioAsistencia');
+
+Route::get('cardex/{id}', function() {
+    return view('cardex');
+})->name('cardex');
 
 Route::get('reporteFaltas/{id}', function($id) {
     return view('reporteFaltas',compact('id'));
@@ -34,4 +38,3 @@ Route::get('/obtener-faltas/{id}', [EmpleadoController::class, 'faltas'])->name(
 
 Route::get('/cardex/{empleadoId}', [EmpleadoController::class, 'mostrarCardex'])->name('cardex');
 
-Route::get('/obtener-faltas', [RegistroController::class, 'validarAsistencia'])->name('validarAsistencia');
